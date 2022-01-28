8b5df5b6329ab164684a40a235346010eba67d5d
Oguzhan Aydin <aydinoguzhan2335@gmail.com>
2022-01-28 18:53:10 +0300

    cam_app

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 const video = document.getElementById('video');
 const btn = document.getElementById("btn");
 const stopbtn= document.getElementById("stopbtn");
+const live = document.getElementById("live");
 
+// buton için event alarak yönlendirme yapıyoruz.
 
 btn.addEventListener("click", ()=>{
     navigator.getUserMedia({
         video:true,
-    }, stream => ( video.srcObject =stream), err => console.log(err))
-    console.log("video açıldı")
+    }, stream => ( video.srcObject =stream), err => console.log(err));
+    console.log("video açıldı");
+    live.style.backgroundColor = "green";
+    btn.style.color="green";
+    stopbtn.style.color="white";
+   
 })
 
-stopbtn.addEventListener("click", () =>{
-    stopbtn.addEventListener("click", ()=>{
-        video.srcObject=null; 
-        console.log("video kapatıldı")
-    })
+stopbtn.addEventListener("click", ()=>{
+    video.srcObject=null; 
+    live.style.backgroundColor = "red";
+    console.log("video kapatıldı");
+    btn.style.color="white";
+    stopbtn.style.color="green";
 })
-
